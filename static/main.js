2d09534af42fbd7dffe1a718691c24e28c9f4ac4
Loic Rosier <87578936+Loicrosier@users.noreply.github.com>
2025-05-19 11:09:12 +0200

    Update main.js

--- static/main.js
+++ static/main.js
@@ -2,30 +2,44 @@ const addBtn = document.querySelector('.add-link')
 
 if (addBtn) {
     addBtn.addEventListener("click", (e) => {
+        if (!document.querySelector('.form-link')) {
+            document.querySelector('.container').classList.add('blur')
+        
+            const formContainer = document.createElement('div')
+            formContainer.classList.add('form-link')
+            formContainer.innerHTML = `
+                                        <div class='close-form'><i class="fa-solid fa-close fa-xl"></i></div>
+                                        <h2>Nouveau lien</h2>
+                                        <form action="/create_link" method="POST">
+                                            <input type='text' class='input-name'name="name" placeholder="nom du lien" autocomplete=off></input>
+                                            <div class='container-link-input'>
+                                                <input type='text' name='link' placeholder='lien' required=true></input>
+                                                <select name="platform" id="platform">
+                                                    <option value="snapchat">Snapchat</option>
+                                                    <option value="telegram">Telegram</option>
+                                                    <option value="whatsapp"> WhatsApp</option>
+                                                    <option value="potato">Potato</option>
+                                                </select>
+                                            </div>
+                                            <input type='text' name="password" class="password" placeholder='mot de passe' required=true></input>
+                                            <input type='submit' class='submit'></input>
+                                        </form>
+                                      `
+            document.body.appendChild(formContainer)
+        }
     
-        const formContainer = document.createElement('div')
-        formContainer.classList.add('form-link')
-        formContainer.innerHTML = `
-                                    <div class='close-form'><i class="fa-solid fa-close fa-xl"></i></div>
-                                    <h2>Nouveau lien</h2>
-                                    <form action="/create_link" method="POST">
-                                        <input type='text' class='input-name'name="name" placeholder="nom du lien" autocomplete=off></input>
-                                        <div class='container-link-input'>
-                                            <input type='text' name='link' placeholder='lien' required=true></input>
-                                            <select name="platform" id="platform">
-                                                <option value="snapchat">Snapchat</option>
-                                                <option value="telegram">Telegram</option>
-                                                <option value="whatsapp"> WhatsApp</option>
-                                                <option value="potato">Potato</option>
-                                            </select>
-                                        </div>
-                                        <input type='text' name="password" class="password" placeholder='mot de passe' required=true></input>
-                                        <input type='submit' class='submit'></input>
-                                    </form>
-                                  `
-        document.body.appendChild(formContainer)
+            // close form
+            const closeBtn = document.querySelector('.close-form')
+            closeBtn.addEventListener('click', (e) => {
+            document.querySelector('.container').classList.remove('blur')
+                document.querySelector('.form-link').remove()
+            })
+            // if (document.querySelector('.close-form')) {
     
-    })
+            // }
+        
+        })
+
 }
 
 
